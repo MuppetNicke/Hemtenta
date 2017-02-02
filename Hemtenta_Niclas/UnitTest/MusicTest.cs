526703947ea1a1c9723a8d0e8d78a4946af4528e
MuppetNicke <niclas-67@hotmail.com>
2017-02-02 22:23:55 +0100

    Music Player progress

--- Hemtenta_Niclas/UnitTest/MusicTest.cs
+++ Hemtenta_Niclas/UnitTest/MusicTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
+using HemtentaTdd2017;
+using HemtentaTdd2017.music;
 
 namespace UnitTest
 {
@@ -8,8 +9,53 @@ namespace UnitTest
     public class MusicTest
     {
         [Test]
-        public void TestMethod1()
+        public void Play()
         {
+            MusicPlayer mp = new MusicPlayer();
+
+            mp.LoadSongs("song");
+
+            mp.Play();
+
+            string result = mp.NowPlaying();
+
+            Assert.That(result, Is.EqualTo("Spelar song1"));
         }
+
+        [Test]
+        public void Play_Fail_Nothing()
+        {
+            MusicPlayer mp = new MusicPlayer();
+
+            mp.Play();
+
+            string result = mp.NowPlaying();
+
+            Assert.That(result, Is.EqualTo("Tystnad råder"));
+        }
+
+        [Test]
+        public void LoadSongs_Succeed()
+        {
+            MusicPlayer mp = new MusicPlayer();
+
+            mp.LoadSongs("song");
+
+            int result = mp.NumSongsInQueue;
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void LoadSongs_Fail_NullReferenceException(string songName)
+        {
+            MusicPlayer mp = new MusicPlayer();
+
+            Assert.Throws<NullReferenceException>(() => mp.LoadSongs(songName));
+        }
+
+
     }
 }
